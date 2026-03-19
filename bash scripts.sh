4e2f650d351e935ead588d9ee64fe7d651724f77
OwenHHHH <owenhandelman@gmail.com>
2026-03-19 01:05:19 -0500

    adding all files to github

--- bash scripts.sh
+++ bash scripts.sh
@@ -14,20 +14,11 @@ poetry run python -m Given_Files.generate_instruction_corpus_mixed `
   --invalid-rate 0.0 `
   --output-mixed data/instruction_corpus.txt
 
-# generating test set pool
-poetry run python -m Given_Files.generate_corpus `
-    --instruction-only `
-    --num-samples 70000 `
-    --max-depth 5 `
-    --num-range 0 99 `
-    --invalid-rate 0.0 `
-    --output-instruction data/test_pool.txt
-
-# using created script to split test pool into specific depth for analysis
-poetry run python -m Data.test_pool_splitter
-
 # creating tokenizer
-!python -m Given_Files.train_tokenizer \
-  --corpus-path Data/foundational_corpus.txt \
-  --output-dir Data \
+!python -m Given_Files.train_tokenizer `
+  --corpus-path Data/foundational_corpus.txt `
+  --output-dir Data `
   --vocab-size 1000
+
+# getting tokenizer data
+!python -m Given_Files.show_token_table
